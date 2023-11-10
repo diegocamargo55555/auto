3d9e2dbc649794031c50fe754a6d047f8defc277
diegocamargo55555 <diegocamargo55555@gmail.com>
2023-11-10 09:47:00 -0300

    Generating a c++ binary_octal_decimal + python folder

---
+++ python/beecrowd/1008.py
@@ -0,0 +1,6 @@
+number = int(input(""))
+horas = int(input(""))
+salario = float(input(""))
+salario_total = horas * salario
+
+print ("NUMBER =", number, "\nSALARY = U$", '{0:.2f}'.format(salario_total))
